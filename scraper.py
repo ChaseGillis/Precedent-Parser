3d9bdc29093c628e6efa4a5887841bd88320a5a0
Chase Gillis <chaseg126@10-17-179-27.dynapool.wireless.nyu.edu>
2023-11-17 15:04:43 -0500

    hello

--- scraper.py
+++ scraper.py
@@ -1,14 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
-soup=BeautifulSoup(requests.get('https://case.law/docs/site_features/api#api-base').text)
-links=[a['href'] for a in soup.find_all('a', {'class':'gnt_m_th_a'})]
-news=''
-for l in links:
-    url='https://case.law/docs/site_features/api#api-base'+l
-    soup=BeautifulSoup(requests.get(url).text)
-    title = soup.find_all('h1',{'class':'gnt_ar_hl'})[0].text
-    author= soup.find_all('a',{'class':'gnt_ar_by_a gnt_ar_by_a__fi'})[0].text
-    date=' '.join(soup.find_all('div',{'class':'gnt_ar_dt'})[0]['aria-label'].split()[4:7])
-    news+=title + ',' +author + ',' + date + ',' + url + '\n'
+import json
 
-#data=json.loads(requests.get('https://nycourts.gov/courthelp/goingtocourt/records.shtml').text)
+'''
+source bin/activate
+
+git add .
+git commit -m “message”
+git push
+'''
+
+def search_cases(keywords, jurisdiction=None):
+    base_url = 'https://api.case.law/v1/cases/?search='
+    search_url = base_url + keywords
+    if jurisdiction:
+        search_url += '&jurisdiction=' + jurisdiction
+    response = requests.get(search_url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+keywords = 'contract law'
+jurisdiction = 'ny'
+
+result = search_cases(keywords, jurisdiction)
+
+newURL=result['results'][0]['url']
+response = requests.get(newURL)
+print(json.loads(str(BeautifulSoup(response.text,'html.parser'))))
+'''
+while ('Opinion' not in newURL):
+    response = requests.get(newURL)
+    soup = BeautifulSoup(response.text,'html.parser')
+    newStuff=json.loads(str(soup))
+    newURL = newStuff['frontend_url']'''
