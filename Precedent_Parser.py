b89010a175f76f7ea353a95fb84a69128aa96708
Kevin Dong <110069590+kevinldong@users.noreply.github.com>
2023-12-07 18:07:05 -0500

    Update Precedent_Parser.py

--- Precedent_Parser.py
+++ Precedent_Parser.py
@@ -1,11 +1,11 @@
-# import libraries
+# import frontend libraries
 import streamlit as st
 from PIL import Image
 import os
 import datetime
 
 
-# backend imports
+# import backend libraries
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -13,18 +13,16 @@ from random import randint
 from openai import OpenAI
 import re
 
+# API keys
+SCRAPEOPS_API_KEY = "[SCRAPEOS API KEY]"
+OPENAI_API_KEY = "[OPENAI API KEY]"
 
-
-
-
-
-
+# frontend code
 # logo images
 logo_image = Image.open(os.path.join(os.path.dirname(__file__), 'logo.PNG'))
 kevin_image = Image.open(os.path.join(os.path.dirname(__file__), 'kevin.png'))
 bryan_image = Image.open(os.path.join(os.path.dirname(__file__), 'bryan.PNG'))
 chase_image = Image.open(os.path.join(os.path.dirname(__file__), 'chase.PNG'))
-
 
 # set up layout
 col1, col2, col3 = st.columns([1.3, 1.3, 1.3])
@@ -36,7 +34,7 @@ with col1:
     st.image(logo_image, width=230)
 
 with col2:
-    st.title('Precedent Parser')  # Adjusted column widths
+    st.title('Precedent Parser')
 
 with col3:
     st.write('')
@@ -47,9 +45,9 @@ with col3:
 
     # display the Team button with dropdown arrow
     if team_open:
-        team_button = st.button('Team \u25B2')  # Upwards pointing arrow when open
+        team_button = st.button('Team \u25B2')
     else:
-        team_button = st.button('Team \u25BC')  # Downwards pointing arrow when closed
+        team_button = st.button('Team \u25BC')
 
     if team_button:
         team_open = not team_open
@@ -58,20 +56,20 @@ with col3:
     if team_open:
         with col1:
             st.image(chase_image, width=200)
-            st.write('My name is Chase Gillis. I am currently a Junior at NYU majoring Computer Science and minoring in Data Science!')
+            st.write('My name is Chase Gillis. I am currently a Junior majoring Computer Science and minoring in Data Science!')
         with col2:
             st.write('')
             st.write('')
             st.write('')
             st.image(bryan_image, width=200)
-            st.write('My name is Bryan Ko. I am currently a Freshman at NYU majoring Computer Science and Data Science, and minoring in Game Design!')
+            st.write('My name is Bryan Ko. I am currently a Freshman majoring Computer Science and Data Science, and minoring in Game Design!')
         with col3:
             i = 0
             while i < 7:
                 st.write('')
                 i += 1
             st.image(kevin_image, width=200)
-            st.write('My name is Kevin Dong. I am currently a Freshman at NYU majoring Math and Computer Science, and minoring in Data Science!')
+            st.write('My name is Kevin Dong. I am currently a Freshman majoring Math and Computer Science, and minoring in Data Science!')
 
 # main content section
 st.header('What cases are you looking for?')
@@ -92,14 +90,7 @@ with col3:
     jurisdiction = st.selectbox('US state:', ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'])
 
 
-
-
-
-
 # backend code
-
-SCRAPEOPS_API_KEY = ""
-
 def get_headers_list():
     response = requests.get('http://headers.scrapeops.io/v1/browser-headers?api_key=' + SCRAPEOPS_API_KEY)
     json_response = response.json()
@@ -248,11 +239,6 @@ def search_cases(keywords, jurisdiction=None):
         print(f"Error: {response.status_code}")
         return None
 
-
-
-
-
-
 outputMessage = ""
 # submit button
 if st.button('Search'):
@@ -278,11 +264,8 @@ if st.button('Search'):
         else:
             allOpinions += bigArray[i] + "\n"
 
-    
-
-
     outputMessage = str(len(allOpinions)) + allOpinions
-    client = OpenAI(api_key="")
+    client = OpenAI(api_key=OPENAI_API_KEY)
 
     response = client.chat.completions.create(
         model="gpt-4-1106-preview",
@@ -291,7 +274,5 @@ if st.button('Search'):
                 }])
     message = response.choices[0].message.content
     outputMessage = message
-    
-
 
 st.text_area(label= "result", value="Enter Keywords and press search!" if outputMessage=="" else outputMessage, height=400, disabled=True, label_visibility="hidden")
