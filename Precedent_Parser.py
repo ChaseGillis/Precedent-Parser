b0dcd578e0db502bda78e0a5372fac31de3175d7
Chase Gillis <chaseg126@Chases-MBP.lan>
2024-06-19 23:40:33 -0400

    Testing keys are working and Fixed urls

--- Precedent_Parser.py
+++ Precedent_Parser.py
@@ -22,6 +22,10 @@ chase_image = Image.open(os.path.join(os.path.dirname(__file__), 'chase.png'))
 # Access the API keys from environment variables
 SCRAPEOPS_API_KEY = os.getenv('SCRAPEOPS_API_KEY')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+if not SCRAPEOPS_API_KEY:
+    raise ValueError("SCRAPEOPS_API_KEY environment variable is not set.")
+if not OPENAI_API_KEY:
+    raise ValueError("OPENAI_API_KEY environment variable is not set.")
 
 
 # Initialize session state for team button
@@ -132,125 +136,85 @@ def cleanhtml(raw_html):
 def search_cases(keywords, jurisdiction=None):
     base_url = 'https://www.courtlistener.com/?type=o&q=contract%20law&type=o&order_by=score%20desc&stat_Precedential=on&filed_after=11%2F15%2F2020&filed_before=12%2F01%2F2023&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag'
     search_url = "https://www.courtlistener.com/api/rest/v3/search/?"
-    # keywords
-    if (len(keywords) > 0):
-        search_url += "q=" + keywords
-    search_url += "&type=o&order_by=score%20desc&stat_Precedential=on"
-    if (after_date != None):
-        year = after_date.strftime("%Y")
-        month = after_date.strftime("%m")
-        day = after_date.strftime("%d")
-        search_url += "&filed_after=" + month + "%2F" + day + "%2F" + year
-    # this can be broken if user makes before come after
-    if (before_date != None):
-        year = before_date.strftime("%Y")
-        month = before_date.strftime("%m")
-        day = before_date.strftime("%d")
-        search_url += "&filed_before=" + month + "%2F" + day + "%2F" + year
-    # Have not yet added in ability for other states besides new york yet
-    match jurisdiction:
-        case "Alabama'":
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Alaska':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Arkansas':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'California':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Colorado':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Connecticut':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Delaware':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'District of Columbia':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Florida':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Georgia':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Hawaii':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Idaho':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Illinois':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Indiana':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Iowa':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Kansas':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Kentucky':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Louisiana':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Maine':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Maryland':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Massachusetts':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Michigan':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Minnesota':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Mississippi':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Missouri':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Montana':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Nebraska':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Nevada':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'New Hampshire':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'New Jersey':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'New Mexico':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'New York':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'North Carolina':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'North Dakota':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Ohio':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Oklahoma':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Oregon':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Pennsylvania':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Rhode Island':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'South Carolina':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'South Dakota':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Tennessee':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Texas':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Utah':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Vermont':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Virginia':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Washington':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'West Virginia':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Wisconsin':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
-        case 'Wyoming':
-            search_url += "&court=nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag"
 
-    print(search_url)
+    # keywords
+    if len(keywords) > 0:
+        search_url += f"q={keywords}"
+
+    search_url += "&type=o&order_by=score%20desc&stat_Precedential=on"
+
+    # after_date and before_date
+    if after_date is not None:
+        after_date_str = after_date.strftime("%m/%d/%Y")
+        search_url += f"&filed_after={after_date_str}"
+
+    if before_date is not None:
+        before_date_str = before_date.strftime("%m/%d/%Y")
+        search_url += f"&filed_before={before_date_str}"
+
+    # jurisdiction
+    if jurisdiction:
+        state_courts = {
+            "Alabama": "almd%20alnd%20alsd%20almd%20almd%20almdb%20alsd%20ald",
+            "Alaska": "akd%20akd%20akd%20akd%20akd%20akd%20akd%20akd%20akd%20akd%20akd%20akd%20akd",
+            "Arizona": "azd%20azd%20azd%20azd%20azd%20azd%20azd%20azd%20azd%20azd%20azd%20azd%20azd",
+            "Arkansas": "ared%20arwd%20ared%20ared%20ared%20ared%20ared%20arwd%20arwd%20arwd%20arwd%20arwd%20arwd",
+            "California": "cand%20cacd%20casd%20cand%20cand%20cand%20cand%20cand%20cand%20cand%20cand%20cand%20cand",
+            "Colorado": "cod%20cod%20cod%20cod%20cod%20cod%20cod%20cod%20cod%20cod%20cod%20cod%20cod",
+            "Connecticut": "ctd%20ctd%20ctd%20ctd%20ctd%20ctd%20ctd%20ctd%20ctd%20ctd%20ctd%20ctd%20ctd",
+            "Delaware": "ded%20ded%20ded%20ded%20ded%20ded%20ded%20ded%20ded%20ded%20ded%20ded%20ded",
+            "District of Columbia": "dcd%20dcd%20dcd%20dcd%20dcd%20dcd%20dcd%20dcd%20dcd%20dcd%20dcd%20dcd%20dcd",
+            "Florida": "fld%20fld%20fld%20fld%20fld%20fld%20fld%20fld%20fld%20fld%20fld%20fld%20fld",
+            "Georgia": "gand%20gand%20gand%20gand%20gand%20gand%20gand%20gand%20gand%20gand%20gand%20gand%20gand",
+            "Hawaii": "hid%20hid%20hid%20hid%20hid%20hid%20hid%20hid%20hid%20hid%20hid%20hid%20hid",
+            "Idaho": "idd%20idd%20idd%20idd%20idd%20idd%20idd%20idd%20idd%20idd%20idd%20idd%20idd",
+            "Illinois": "ild%20ilnd%20ilcd%20ild%20ild%20ild%20ild%20ild%20ild%20ild%20ild%20ild%20ild",
+            "Indiana": "ind%20ind%20ind%20ind%20ind%20ind%20ind%20ind%20ind%20ind%20ind%20ind%20ind",
+            "Iowa": "iad%20iasd%20iasd%20iad%20iad%20iad%20iad%20iad%20iad%20iad%20iad%20iad%20iad",
+            "Kansas": "ksd%20ksd%20ksd%20ksd%20ksd%20ksd%20ksd%20ksd%20ksd%20ksd%20ksd%20ksd%20ksd",
+            "Kentucky": "kyd%20kyd%20kyd%20kyd%20kyd%20kyd%20kyd%20kyd%20kyd%20kyd%20kyd%20kyd%20kyd",
+            "Louisiana": "lad%20lad%20lad%20lad%20lad%20lad%20lad%20lad%20lad%20lad%20lad%20lad%20lad",
+            "Maine": "med%20med%20med%20med%20med%20med%20med%20med%20med%20med%20med%20med%20med",
+            "Maryland": "mdd%20mdd%20mdd%20mdd%20mdd%20mdd%20mdd%20mdd%20mdd%20mdd%20mdd%20mdd%20mdd",
+            "Massachusetts": "mad%20mad%20mad%20mad%20mad%20mad%20mad%20mad%20mad%20mad%20mad%20mad%20mad",
+            "Michigan": "mid%20mid%20mid%20mid%20mid%20mid%20mid%20mid%20mid%20mid%20mid%20mid%20mid",
+            "Minnesota": "mnd%20mnd%20mnd%20mnd%20mnd%20mnd%20mnd%20mnd%20mnd%20mnd%20mnd%20mnd%20mnd",
+            "Mississippi": "msnd%20mssd%20mssd%20msnd%20msnd%20msnd%20msnd%20msnd%20msnd%20msnd%20msnd%20msnd%20msnd",
+            "Missouri": "moed%20moed%20moed%20moed%20moed%20moed%20moed%20moed%20moed%20moed%20moed%20moed%20moed",
+            "Montana": "mtd%20mtd%20mtd%20mtd%20mtd%20mtd%20mtd%20mtd%20mtd%20mtd%20mtd%20mtd%20mtd",
+            "Nebraska": "ned%20ned%20ned%20ned%20ned%20ned%20ned%20ned%20ned%20ned%20ned%20ned%20ned",
+            "Nevada": "nvd%20nvd%20nvd%20nvd%20nvd%20nvd%20nvd%20nvd%20nvd%20nvd%20nvd%20nvd%20nvd",
+            "New Hampshire": "nhd%20nhd%20nhd%20nhd%20nhd%20nhd%20nhd%20nhd%20nhd%20nhd%20nhd%20nhd%20nhd",
+            "New Jersey": "njd%20njd%20njd%20njd%20njd%20njd%20njd%20njd%20njd%20njd%20njd%20njd%20njd",
+            "New Mexico": "nmd%20nmd%20nmd%20nmd%20nmd%20nmd%20nmd%20nmd%20nmd%20nmd%20nmd%20nmd%20nmd",
+            "New York": "nyed%20nynd%20nysd%20nywd%20nyeb%20nynb%20nysb%20nywb%20ny%20nyappdiv%20nyappterm%20nysupct%20nyfamct%20nysurct%20nycivct%20nycrimct%20nyag",
+            "North Carolina": "nced%20nced%20nced%20nced%20nced%20nced%20nced%20nced%20nced%20nced%20nced%20nced%20nced",
+            "North Dakota": "ndd%20ndd%20ndd%20ndd%20ndd%20ndd%20ndd%20ndd%20ndd%20ndd%20ndd%20ndd%20ndd",
+            "Ohio": "ohnd%20ohnd%20ohnd%20ohnd%20ohnd%20ohnd%20ohnd%20ohnd%20ohnd%20ohnd%20ohnd%20ohnd%20ohnd",
+            "Oklahoma": "okwd%20okwd%20okwd%20okwd%20okwd%20okwd%20okwd%20okwd%20okwd%20okwd%20okwd%20okwd%20okwd",
+            "Oregon": "ord%20ord%20ord%20ord%20ord%20ord%20ord%20ord%20ord%20ord%20ord%20ord%20ord",
+            "Pennsylvania": "pad%20pad%20pad%20pad%20pad%20pad%20pad%20pad%20pad%20pad%20pad%20pad%20pad",
+            "Rhode Island": "rid%20rid%20rid%20rid%20rid%20rid%20rid%20rid%20rid%20rid%20rid%20rid%20rid",
+            "South Carolina": "scd%20scd%20scd%20scd%20scd%20scd%20scd%20scd%20scd%20scd%20scd%20scd%20scd",
+            "South Dakota": "sdd%20sdd%20sdd%20sdd%20sdd%20sdd%20sdd%20sdd%20sdd%20sdd%20sdd%20sdd%20sdd",
+            "Tennessee": "tnmd%20tnmd%20tnmd%20tnmd%20tnmd%20tnmd%20tnmd%20tnmd%20tnmd%20tnmd%20tnmd%20tnmd%20tnmd",
+            "Texas": "txed%20txnd%20txsd%20txwd%20txed%20txed%20txed%20txed%20txnd%20txsd%20txwd%20txed%20txed",
+            "Utah": "utd%20utd%20utd%20utd%20utd%20utd%20utd%20utd%20utd%20utd%20utd%20utd%20utd",
+            "Vermont": "vtd%20vtd%20vtd%20vtd%20vtd%20vtd%20vtd%20vtd%20vtd%20vtd%20vtd%20vtd%20vtd",
+            "Virginia": "vad%20vad%20vad%20vad%20vad%20vad%20vad%20vad%20vad%20vad%20vad%20vad%20vad",
+            "Washington": "wad%20wad%20wad%20wad%20wad%20wad%20wad%20wad%20wad%20wad%20wad%20wad%20wad",
+            "West Virginia": "wvsd%20wvsd%20wvsd%20wvsd%20wvsd%20wvsd%20wvsd%20wvsd%20wvsd%20wvsd%20wvsd%20wvsd%20wvsd",
+            "Wisconsin": "wid%20wid%20wid%20wid%20wid%20wid%20wid%20wid%20wid%20wid%20wid%20wid%20wid",
+            "Wyoming": "wyd%20wyd%20wyd%20wyd%20wyd%20wyd%20wyd%20wyd%20wyd%20wyd%20wyd%20wyd%20wyd",
+            "Guam": "gud%20gud%20gud%20gud%20gud%20gud%20gud%20gud%20gud%20gud%20gud%20gud%20gud",
+            "Northern Mariana Islands": "mpd%20mpd%20mpd%20mpd%20mpd%20mpd%20mpd%20mpd%20mpd%20mpd%20mpd%20mpd%20mpd",
+            "Puerto Rico": "prd%20prd%20prd%20prd%20prd%20prd%20prd%20prd%20prd%20prd%20prd%20prd%20prd",
+            "U.S. Virgin Islands": "vid%20vid%20vid%20vid%20vid%20vid%20vid%20vid%20vid%20vid%20vid%20vid%20vid",
+        }
+
+        if jurisdiction in state_courts:
+            search_url += f"&court={state_courts[jurisdiction]}"
+
     response = requests.get(search_url)
     if response.status_code == 200:
         data = response.json()
